f4b0f841686940084167cec3516480f22ff96d71
Felix-Jacob <149216340+Felix-Jacob@users.noreply.github.com>
2024-06-27 00:51:19 -0400

    Add UI to game. Create 'replay' button.

--- script.js
+++ script.js
@@ -1,45 +1,76 @@
-let choice= ["rock", "paper", "scissors"];
+let choice=["rock", "paper", "scissors"];
 let humanScore=0;
 let computerScore=0;
-
-gameLoop();
+let humanScoreSpan= document.querySelector('#human-score-span');
+let computerScoreSpan= document.querySelector('#computer-score-span');
+let displayMsg= document.querySelector('#display-msg');
+let humanChoice=""
+let choiceDiv= document.querySelector('#choice');
+let replayBtn= document.querySelector('#replay');
+/*gameLoop();
 
 function gameLoop(){
     while (humanScore<5 && computerScore<5){
         (playRound(getHumanChoice(),getComputerChoice()));
     } console.log(humanScore===5? "You win the game!":"You lost the game...");
-
-}
+}*/
 function getComputerChoice(){
     let computerChoice=choice[Math.floor(Math.random()*choice.length)];
     return computerChoice;
 }
 
-function getHumanChoice(){
-    let humanChoice= prompt("Please choose 'rock', 'paper', or 'scissors'.");
-    return humanChoice = choice.includes(humanChoice.toLowerCase())? humanChoice.toLowerCase(): "This was not one of the options.";
+
+choiceDiv.addEventListener('click', (event)=>{
+    let target= event.target;
+
+    switch(target.id) {
+        case 'rock':
+            humanChoice='rock';
+            break;
+          case 'paper':
+            humanChoice='paper';
+            break;
+        case 'scissors':
+            humanChoice="scissors";
+            break;
+    }
+    console.log(humanChoice)
+    playRound(humanChoice,getComputerChoice())
+})
+
+function endGame(){
+    humanScore===5? displayMsg.textContent+=`You have won the game! Play again?`:displayMsg.textContent+=`You have lost the game. Try again?`;
+    choiceDiv.style.display='none';
+    replayBtn.style.display='block';
+    replayBtn.addEventListener('click',reset);
+}
+
+function reset(){
+    humanScore=0;
+    computerScore=0;
+    humanScoreSpan.textContent='0';
+    computerScoreSpan.textContent='0';
+    displayMsg.textContent='Rock, Paper, Scissors, Shoot!';
+    replayBtn.style.display='none';
+    choiceDiv.style.display='block';
 }
 
 function playRound(humanChoice, computerChoice){
-    if (choice.includes(humanChoice)){
-        while (humanChoice === computerChoice) {
+        while (humanChoice === computerChoice){
            computerChoice = getComputerChoice();
         }
-           let result = (humanChoice==="rock" && computerChoice === "scissors"  || humanChoice === "scissors"  && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock"|| false);
-        
-          
-          if (result){
-           humanScore++;
-            console.log(`You Win! ${humanChoice} beats ${computerChoice}.
-            The score is now:
-            Human:${humanScore} Computer:${computerScore}`);
+        let result = (humanChoice==="rock" && computerChoice === "scissors"  || humanChoice === "scissors"  && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock"|| false);
+        if (result){
+            humanScore++;
+            displayMsg.textContent=`You Win! ${humanChoice} beats ${computerChoice}.`;
+            humanScoreSpan.textContent=humanScore;
         } else {
             computerScore++;
-            console.log(`Computer Wins. ${computerChoice} beats ${humanChoice}.    
-            The score is now:
-            Human:${humanScore} Computer:${computerScore}`);
+            displayMsg.textContent=`Computer Wins. ${computerChoice} beats ${humanChoice}.`;
+            computerScoreSpan.textContent=computerScore;
         }
-
-    } else {console.log("Your entry is invalid.");}
-}
+        if (computerScore ===5 || humanScore===5){
+            endGame();
+        }
+    }
 
